49af3c04708e89c5b1e72c97df063373624cc09e
nick4633 <nickyoukhana@outlook.com>
2022-02-15 14:46:23 +1300

    adding content to website

--- main_app.py
+++ main_app.py
@@ -1,12 +1,36 @@
 from flask import Flask,render_template
-
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'nick youkhana',
+        'title': 'blog post 1',
+        'content':'first post content',
+        'date_posted':'feb 14, 2022'
+    },
+    
+    {
+        'author': 'jonny cassh',
+        'title': 'blog post 2',
+        'content':'second post content',
+        'date_posted':'feb 14, 2022'
+        
+    },
+    
+    {
+        'author': 'cheese supreme',
+        'title': 'blog post 3',
+        'content':'third post content',
+        'date_posted':'feb 14, 2022'
+        
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
